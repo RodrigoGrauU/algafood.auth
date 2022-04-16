462dc0c2ebb4248dc4a48b200d3ddd65ceeb3288
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-04-16 10:10:38 -0300

    22.19. Testando o fluxo Authorization Code com um client JavaScript

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -42,7 +42,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.secret(passwordEncoder.encode("food123"))
 				.authorizedGrantTypes("authorization_code")
 				.scopes("write", "read")
-				.redirectUris("http://aplicacao-cliente")
+				.redirectUris("http://localhost:8000")
 			
 			.and()
 				.withClient("faturamento")
